46be8329bb5fb8ab7ebeccea412c22b3d3b109db
Mae.reliable <Mae.reliable@gmail.com>
2019-02-18 10:58:39 +0800

    Initial Commit

---
+++ Layer和View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Layer和View
+//
+//  Created by Mae on 2019/2/18.
+//  Copyright © 2019年 刘静冉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Layer和View/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Layer和View
+//
+//  Created by Mae on 2019/2/18.
+//  Copyright © 2019年 刘静冉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
